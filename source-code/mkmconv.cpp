c13c2743dbc4cf0cc0cb561b7ce800796396796e
tventea <tahayerd@proton.me>
2024-08-15 03:02:27 +0300

    Version 1.0 OUT!!!111!!11!!!

---
+++ source-code/mkmconv.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+int main(){
+        using std::cout;
+        using std::cin;
+
+    double uzunluk;
+    char birim;
+
+    cout << "******MIL TO KM CONVERTER*****\n";
+
+    while (true) {
+    cout << "M=Mile\n";
+    cout << "K=Kilometer\n";
+    cout << "Which unit of length do you want to convert it to?: ";
+    cin >> birim;
+
+    if (birim == 'K' || birim == 'k')
+    {
+        cout << "Enter the length as a mile: ";
+        cin >> uzunluk;
+        double km = uzunluk * 1.60934;
+        cout << "The lenght is " << km << " kilometer.\n"; 
+        break;
+    }
+    else if (birim == 'M' || birim == 'm')
+    {
+        cout << "Enter the length as a kilometer: \n";
+        cin >> uzunluk;
+        double mil = uzunluk / 1.60934;
+        cout << "The length is " << mil << " mile.\n";
+        break;
+    }
+    else{
+        cout << "Please enter either K or M units.\n";
+        
+        
+    }
+    cout << "******************************\n";
+    }
+    
+
+
+
+
+    cout << "*****************************\n";
+
+
+  system("pause");
+  return 0;
+}
